118d1fc757f035037aa5b6d3246952cc04f21ddb
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-06-18 19:36:56 +0200

    feat: Secure secrets

--- util/auth.js
+++ util/auth.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
+require("dotenv").config();
 
-const API_KEY = "AIzaSyBcr1gE6OHEcM2pVrBBDEk-9-HR7q4M9fE"
 
 async function createUser(email, password) {
   const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + process.env.API_KEY,
     {
       email: email,
       password: password,
