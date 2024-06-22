c8e539a5ccebc1274487396aadba23cffcf6224d
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-06-22 16:27:41 +0200

    creating New Users

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 import AuthContent from '../components/Auth/AuthContent';
+import { createUser } from '../util/auth';
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setisAuthenticating] = useState(false);
+
+  // ______________________________________________________________________
+  async function signupHandler({ email, password }) {
+    setisAuthenticating(true);
+    await createUser(email, password);
+    setisAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
