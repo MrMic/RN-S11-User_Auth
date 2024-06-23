9c4e0257c8cd0c0ff9cbff0b2c78821bf81f90ce
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-06-23 11:42:18 +0200

    feat(UI/UX): Log Users in

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 import AuthContent from '../components/Auth/AuthContent';
+import { login } from '../util/auth';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setisAuthenticating] = useState(false);
+
+  // ______________________________________________________________________
+  async function loginHandler({ email, password }) {
+    setisAuthenticating(true);
+    await login(email, password);
+    setisAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Login you in..." />
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
